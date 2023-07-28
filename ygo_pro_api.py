7ab2cb17f44ad83ff4d96fefb2288cc998ea9c58
rushdawg <rushdawg@rushdawgs-MacBook-Pro.local>
2023-07-28 00:32:47 -0400

    remove unused function

--- ygo_pro_api.py
+++ ygo_pro_api.py
@@ -15,16 +15,6 @@ def get_all_cards() -> list[dict[str, Any]]:
         log.exception(f"Failed to get information from ygoprodeck with exception:\n\n {e}")
     return data
 
-def _get_card_image_links() -> list[str]:
-     response = requests.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-     data = response.json()['data']
-     card_image_links = []
-     for card in data:
-        image_links = card['card_images']
-        for image_link in image_links:
-            card_image_links.append(image_link['image_url'])
-     return card_image_links
-    
 def request_image(image_url: str, file_name: str): 
     print(image_url)
     response = requests.get(image_url, stream=True)
